70e9f455f01c654868c2dded25d03d640e1b5c40
Marc Bruins <marc@marcbruins.nl>
2017-04-11 15:42:24 +0200

    add baseclient implemation

--- src/Xablu.WebApiClient/BaseClient.cs
+++ src/Xablu.WebApiClient/BaseClient.cs
@@ -1,10 +1,47 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Polly;
+using System.Net;
+
 namespace Xablu.WebApiClient
 {
-    public class BaseClient
-    {
-        public BaseClient()
-        {
-        }
-    }
+	public class BaseClient
+	{
+		protected readonly IWebApiClient apiClient;
+
+		public BaseClient(IWebApiClient apiClient)
+		{
+			if (apiClient == null)
+				throw new ArgumentNullException(nameof(apiClient));
+
+			this.apiClient = apiClient;
+		}
+
+		protected async Task<TResult> ExecuteRemoteRequest<TResult>(Func<Task<TResult>> action)
+		{
+			TResult result = default(TResult);
+
+			try
+			{
+				result = await Policy
+					.Handle<WebException>()
+					.WaitAndRetryAsync
+					(
+						retryCount: 5,
+						sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))
+					)
+					.ExecuteAsync(action).ConfigureAwait(false);
+			}
+			catch (Exception e)
+			{
+				if (e.GetType().Namespace == "Java.Net")
+				{
+					throw new HttpExtensions.HttpResponseException(HttpStatusCode.RequestTimeout, e.Message, e.StackTrace);
+				}
+				throw;
+			}
+
+			return result;
+		}
+	}
 }
