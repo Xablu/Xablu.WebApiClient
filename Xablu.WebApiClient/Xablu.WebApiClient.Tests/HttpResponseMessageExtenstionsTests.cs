91e8ebdb5b9d827ffb3fd83f3e0f7eb3e72ba729
Martijn van Dijk <mhvdijk@gmail.com>
2017-09-22 14:27:56 +0200

    Add unit test project

---
+++ Xablu.WebApiClient/Xablu.WebApiClient.Tests/HttpResponseMessageExtenstionsTests.cs
@@ -0,0 +1,105 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xablu.WebApiClient.HttpExtensions;
+using Xablu.WebApiClient.UnitTests.Mocks;
+using Xunit;
+
+namespace Xablu.WebApiClient.UnitTests
+{
+    public class HttpResponseMessageExtenstionsTests
+    {
+        [Fact]
+        public async Task BuildRestApiResult_SuccessResponse_CallResolveOnHttpResponseResolverOnce()
+        {
+            var mockResolver = new MockHttpResponseResolver();
+            var responseMessage = new HttpResponseMessage(HttpStatusCode.OK);
+
+            var result = await responseMessage.BuildRestApiResult<string>(mockResolver);
+
+            Assert.Equal(1, mockResolver.CallsToResolveHttpResponse);
+        }
+
+        [Fact]
+        public async Task BuildRestApiResult_SuccessResponse_ResultStatusCodeMatchesResponseStatusCode()
+        {
+            var mockResolver = new MockHttpResponseResolver();
+            var responseMessage = new HttpResponseMessage(HttpStatusCode.OK);
+
+            var result = await responseMessage.BuildRestApiResult<string>(mockResolver);
+
+            Assert.Equal(responseMessage.StatusCode, result.HttpStatusCode);
+        }
+
+        [Fact]
+        public async Task BuildRestApiResult_SuccessResponse_ResultReasonPhraseMatchesResponseReasonPhrase()
+        {
+            var mockResolver = new MockHttpResponseResolver();
+            var responseMessage = new HttpResponseMessage(HttpStatusCode.OK);
+
+            var result = await responseMessage.BuildRestApiResult<string>(mockResolver);
+
+            Assert.Equal(responseMessage.ReasonPhrase, result.ReasonPhrase);
+        }
+
+        [Fact]
+        public async Task BuildRestApiResult_FailedResponse_NoCallToResolveOnHttpResponseResolver()
+        {
+            var mockResolver = new MockHttpResponseResolver();
+            var responseMessage = new HttpResponseMessage(HttpStatusCode.NotFound);
+
+            var result = await responseMessage.BuildRestApiResult<string>(mockResolver);
+
+            Assert.Equal(0, mockResolver.CallsToResolveHttpResponse);
+        }
+
+        [Fact]
+        public async Task BuildRestApiResult_FailedResponse_ResultStatusCodeMatchesResponseStatusCode()
+        {
+            var mockResolver = new MockHttpResponseResolver();
+            var responseMessage = new HttpResponseMessage(HttpStatusCode.NotFound);
+
+            var result = await responseMessage.BuildRestApiResult<string>(mockResolver);
+
+            Assert.Equal(result.HttpStatusCode, responseMessage.StatusCode);
+        }
+
+        [Fact]
+        public async Task BuildRestApiResult_FailedResponse_ResultReasonPhraseMatchesResponseReasonPhrase()
+        {
+            var mockResolver = new MockHttpResponseResolver();
+            var responseMessage = new HttpResponseMessage(HttpStatusCode.NotFound);
+
+            var result = await responseMessage.BuildRestApiResult<string>(mockResolver);
+
+            Assert.Equal(result.ReasonPhrase, responseMessage.ReasonPhrase);
+        }
+
+        [Fact]
+        public async Task BuildRestApiResult_FailedResponseWithNoContent_ResultDetailedErrorMessageIsEmpty()
+        {
+            var mockResolver = new MockHttpResponseResolver();
+            var responseMessage = new HttpResponseMessage(HttpStatusCode.NotFound);
+
+            var result = await responseMessage.BuildRestApiResult<string>(mockResolver);
+
+            Assert.Equal(string.Empty, result.DetailedErrorMessage);
+        }
+
+        [Fact]
+        public async Task BuildRestApiResult_FailedResponseWithContent_ResultDetailedErrorMessageMatchesContent()
+        {
+            const string expectedErrorMessage = "Very detailed error message";
+            var mockResolver = new MockHttpResponseResolver();
+            var responseMessage =
+                new HttpResponseMessage(HttpStatusCode.NotFound)
+                {
+                    Content = new StringContent(expectedErrorMessage)
+                };
+
+            var result = await responseMessage.BuildRestApiResult<string>(mockResolver);
+
+            Assert.Equal(expectedErrorMessage, result.DetailedErrorMessage);
+        }
+    }
+}
---
+++ Xablu.WebApiClient/Xablu.WebApiClient.Tests/Mocks/MockHttpResponseResolver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xablu.WebApiClient.UnitTests.Mocks
+{
+    internal class MockHttpResponseResolver : IHttpResponseResolver
+    {
+        public int CallsToResolveHttpResponse { get; private set; }
+
+        public Task<TResult> ResolveHttpResponseAsync<TResult>(HttpResponseMessage responseMessage)
+        {
+            CallsToResolveHttpResponse++;
+
+            return Task.FromResult(default(TResult));
+        }
+    }
+}
