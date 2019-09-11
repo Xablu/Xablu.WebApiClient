7686ea458f530f8bb5ec6fa81f9327ea0fe15acf
Luuk <luuk_jongebloet2@hotmail.com>
2019-09-11 21:45:21 +0200

    Refactored Request class this includes:
    - Cleaned up overal code
    - Rewritten most methods
    - Changed Linq query syntax -> method syntax

--- src/Xablu.WebApiClient/Services/GraphQL/Request.cs
+++ src/Xablu.WebApiClient/Services/GraphQL/Request.cs
@@ -12,135 +12,120 @@ namespace Xablu.WebApiClient.Services.GraphQL
 {
     public class Request<T> : GraphQLRequest
     {
-        private List<Dictionary<string, object>> ObjectPropsList = new List<Dictionary<string, object>>();
+        private List<Dictionary<string, object>> PropertyDictList = new List<Dictionary<string, object>>();
 
         int attributeNumber;
 
         public Request(string query = "", object model = null, string[] optionalParameters = null)
         {
-            if (model != null)
-            {
-                ResponseModel = (T)model;
-                if (ResponseModel != null)
-                {
-                    var QueryString = GetQueryString(ResponseModel);
+            OptionalParameters = optionalParameters;
+            Query = query;
 
-                    if (optionalParameters != null)
-                    {
-                        QueryString = FormatQuery(QueryString, optionalParameters);
-                    }
-
-                    GraphQLQuery = QueryString;
-                    Query = GraphQLQuery;
-                }
-
-            }
-            else
-            {
-                Query = query;
-                GraphQLQuery = Query;
-            }
-        }
-
-        private string GetQueryString(T obj)
-        {
-            //need help
-            Type type = obj.GetType();
-            Iterate(type);
-
-            var result = CreateQuery();
-            return result;
+            CreateQuery(model);
         }
 
         public string GraphQLQuery { get; set; }
 
         public T ResponseModel { get; set; }
 
-        private void Iterate(Type type)
+        public string[] OptionalParameters { get; set; }
+
+
+        public void CreateQuery(object model)
+        {
+            if (model != null)
+            {
+                //which one is prefered?
+                ResponseModel = model.GetType() is T ? (T)model : default;
+
+                ResponseModel = (T)model;
+
+                if (ResponseModel != null)
+                {
+                    var queryString = GetQuery(ResponseModel);
+                    GraphQLQuery = queryString;
+                    Query = GraphQLQuery;
+                }
+            }
+            else
+            {
+                GraphQLQuery = Query;
+            }
+        }
+
+
+        private string GetQuery(T obj)
+        {
+            GetProperties(obj.GetType());
+
+            var unformattedQuery = QueryBuilder();
+
+            var result = OptionalParameters != null ? FormatQuery(unformattedQuery, OptionalParameters) : unformattedQuery;
+
+            return result;
+        }
+
+        private void GetProperties(Type type)
         {
 
-            var props = new List<PropertyInfo>();
+            var propsList = new List<PropertyInfo>();
             var baseType = type;
-            var propNames = new HashSet<object>();
             var propDict = new Dictionary<string, object>();
-
 
             while (baseType != typeof(object))
             {
-                var ti = baseType.GetTypeInfo();
-
-                var newProps = (
-                    from p in ti.DeclaredProperties
-                    where
-                        !propNames.Contains(p.Name) &&
-                        p.CanRead && p.CanWrite &&
-                        (p.GetMethod != null) && (p.SetMethod != null) &&
-                        (p.GetMethod.IsPublic && p.SetMethod.IsPublic) &&
-                        (!p.GetMethod.IsStatic) && (!p.SetMethod.IsStatic)
-                    select p).ToList();
-
-                props.AddRange(newProps);
-                baseType = ti.BaseType;
+                var typeInfo = baseType.GetTypeInfo();
+                var newProps = typeInfo.DeclaredProperties.Where(p => !propDict.ContainsKey(p.Name) &&
+                                                                p.CanRead && p.CanWrite &&
+                                                                (p.GetMethod != null) && (p.SetMethod != null) &&
+                                                                (p.GetMethod.IsPublic && p.SetMethod.IsPublic) &&
+                                                                (!p.GetMethod.IsStatic) && (!p.SetMethod.IsStatic)).ToList();
+                propsList.AddRange(newProps);
+                baseType = typeInfo.BaseType;
             }
 
-            foreach (PropertyInfo property in props)
+            foreach (PropertyInfo property in propsList)
             {
                 var propType = property.PropertyType;
                 string propName = property.Name;
 
-
-                var HasAttribute = Attribute.IsDefined(property, typeof(QueryParameter));
-
-                if (HasAttribute)
+                var hasAttribute = Attribute.IsDefined(property, typeof(QueryParameter));
+                if (hasAttribute)
                 {
-                    propNames.Add($"{property.Name}" + $"{{{attributeNumber.ToString()}}}");
                     propDict.Add($"{property.Name}" + $"{{{attributeNumber.ToString()}}}", null);
-
                     attributeNumber++;
                 }
                 else
                 {
-                    propNames.Add(property.Name);
                     propDict.Add(property.Name, null);
                 }
 
-
-
                 if (propType.IsClass)
                 {
-                    var test = propType.GetProperties();
                     var hasProperties = propType.GetProperties() != null && propType.GetProperties().Length > 0;
                     if (hasProperties)
                     {
-                        Iterate(propType);
+                        GetProperties(propType);
                     }
                 }
             }
+
             if (propDict.Any())
             {
-                ObjectPropsList.Add(propDict);
+                PropertyDictList.Add(propDict);
             }
         }
 
-        private string CreateQuery()
+        private string QueryBuilder()
         {
             string queryString = "";
 
-            foreach (Dictionary<string, object> propDic in ObjectPropsList)
+            foreach (Dictionary<string, object> propertyDictonary in PropertyDictList)
             {
-                foreach (KeyValuePair<string, object> property in propDic.Reverse())
+                foreach (KeyValuePair<string, object> property in propertyDictonary.Reverse())
                 {
-                    if (queryString.Count() > 0)
-                    {
-                        queryString = queryString.Insert(0, property.Key.ToLower() + " ");
-                    }
-                    else
-                    {
-                        queryString = queryString.Insert(0, property.Key.ToLower());
-                    }
-
+                    queryString = queryString.Any() ? queryString = queryString.Insert(0, property.Key.ToLower() + " ") : queryString.Insert(0, property.Key.ToLower());
                 }
-
                 queryString = "{{" + $"{queryString}" + "}}";
             }
             return queryString;
