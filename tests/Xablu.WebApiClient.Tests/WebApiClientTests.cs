84fcd2e1d91298e758eeb196044fa5d8eb27e556
nmilcoff <nicolasmilcoff@gmail.com>
2019-09-12 21:05:00 -0300

    Add two first tests for WebApiClient

---
+++ tests/Xablu.WebApiClient.Tests/WebApiClientTests.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Threading.Tasks;
+using Moq;
+using Refit;
+using Xablu.WebApiClient.Client;
+using Xunit;
+
+namespace Xablu.WebApiClient.Tests
+{
+    public interface IStarwarsApi
+    {
+        [Get("/starships")]
+        Task Get();
+    }
+
+    public class WebApiClientTests
+    {
+        [Fact]
+        public void EnsureUserInitiatedIsSelectedByDefault()
+        {
+            // mock dependencies 
+            var service = new Mock<IRefitService<IStarwarsApi>>();
+            var mockedAPI = new Mock<IStarwarsApi>();
+
+            // setup dependency mocks
+            service.SetupGet(s => s.UserInitiated).Returns(mockedAPI.Object);
+            mockedAPI.Setup(api => api.Get()).Returns(Task.CompletedTask).Verifiable();
+
+            var client = new WebApiClient<IStarwarsApi>(service.Object);
+
+            // execute sentence to test
+            client.Call(api => api.Get());
+
+            // make sure UserInitiated was called
+            service.VerifyGet(s => s.UserInitiated);
+            mockedAPI.VerifyAll();
+        }
+
+        [Fact]
+        public void EnsureOperationIsCalledInTheRefitAPI()
+        {
+            // mock dependencies 
+            var service = new Mock<IRefitService<IStarwarsApi>>();
+            var mockedAPI = new Mock<IStarwarsApi>();
+
+            // setup dependency mocks
+            service.SetupGet(s => s.UserInitiated).Returns(mockedAPI.Object);
+            mockedAPI.Setup(api => api.Get()).Returns(Task.CompletedTask).Verifiable();
+
+            var client = new WebApiClient<IStarwarsApi>(service.Object);
+
+            // execute sentence to test
+            client.Call(api => api.Get());
+
+            // make sure IStarwarsApi.Get was called
+            mockedAPI.VerifyAll();
+        }
+    }
+}
