5a6766b5cc432d1fc971a96b082823d1c5a793cc
nmilcoff <nicolasmilcoff@gmail.com>
2019-11-08 12:12:20 -0300

    Added NativeHttpClientHandlers for every platform and implement these for GraphQL and Refit calls

---
+++ src/Xablu.WebApiClient/Native/NativeHttpClientHandler.ios.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Xablu.WebApiClient.Native
+{
+    public class NativeHttpClientHandler : NSUrlSessionHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
