3d3096d054b4eb2ee64f29225d5bb52e33b99da5
Luuk <luuk_jongebloet2@hotmail.com>
2020-01-28 23:30:59 +0100

    no message

---
+++ Samples/BooksQL.API/GraphQL/Types/BookReviewInputType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using GraphQL.Types;
+
+namespace BooksQL.API.GraphQL.Types
+{
+    public class BookReviewInputType : InputObjectGraphType
+    {
+        public BookReviewInputType()
+        {
+            Name = "reviewInput";
+            Field<NonNullGraphType<StringGraphType>>("bookISBN");
+            Field<NonNullGraphType<StringGraphType>>("review");
+        }
+    }
+}
---
+++ Samples/BooksQL.API/GraphQL/Types/BookReviewType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using BooksQL.API.Entities;
+using GraphQL.Types;
+
+namespace BooksQL.API.GraphQL.Types
+{
+    public class BookReviewType : ObjectGraphType<BookReview>
+    {
+        public BookReviewType()
+        {
+            Field(t => t.BookISBN);
+            Field(t => t.Id);
+            Field(t => t.Review);
+        }
+    }
+}
